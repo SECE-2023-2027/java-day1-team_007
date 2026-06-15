267c0d99023326fbde80de9b593bfb3ca3ac3738
majhus001 <majidsmart7@gmail.com>
2026-06-15 15:49:22 +0530

    Add Test1.java to greet user input

---
+++ majid/Test1.java
@@ -0,0 +1,11 @@
+package majid;
+import java.util.*;
+
+class Test1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        System.out.println("Hello! "+name);
+    }
+}
