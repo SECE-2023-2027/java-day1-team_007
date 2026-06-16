770b2c8377ed095276c6a2a0721dd6a6017e354e
Shri Ramakrishnan A <ramvaisu.m@gmail.com>
2026-06-16 07:36:36 +0530

    Test3

---
+++ ram/Test3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Circle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        double radius, area, perimeter;
+
+        System.out.print("Enter radius: ");
+        radius = sc.nextDouble();
+
+        area = 3.14 * radius * radius;
+        perimeter = 2 * 3.14 * radius;
+
+        System.out.println("Area = " + area);
+        System.out.println("Perimeter = " + perimeter);
+    }
+}
